3996901eb8fb5fa666cddc96b2d1c4020c9ed0b4
mijicstefan <stefanmijic30@gmail.com>
2021-04-09 00:32:18 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Stefi Car Najveci newww
         </p>
         <a
           className="App-link"
